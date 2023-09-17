2e24b2148f2e6828af6adffac70b7e1a447774c5
Domitius <133902698+recenasu@users.noreply.github.com>
2023-09-17 16:53:32 -0700

    added api routes

--- models/index.js
+++ models/index.js
@@ -19,7 +19,7 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
-    model: 'ProductTag',
+    model: ProductTag,
     unique: false
   },
 })
